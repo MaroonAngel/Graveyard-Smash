76e012a3fcf595058fc651811342ef137d19b394
Jamie <54751722+MaroonAngel@users.noreply.github.com>
2020-10-10 12:21:27 -0600

    carved pumpkin block

--- src/main/java/net/jam/gravesmash/block/CarvedPumpkinBlock.java
+++ src/main/java/net/jam/gravesmash/block/CarvedPumpkinBlock.java
@@ -4,12 +4,13 @@ import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.HorizontalFacingBlock;
 import net.minecraft.block.ShapeContext;
+import net.minecraft.item.ItemPlacementContext;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.shape.VoxelShape;
 import net.minecraft.world.BlockView;
 
-public class WhitePumpkinBlock extends ResizedPumpkinBlock {
-    public WhitePumpkinBlock(Settings settings) {
+public class CarvedPumpkinBlock extends HorizontalFacingBlock {
+    protected CarvedPumpkinBlock(Settings settings) {
         super(settings);
     }
 
@@ -18,5 +19,7 @@ public class WhitePumpkinBlock extends ResizedPumpkinBlock {
         return Block.createCuboidShape(4, 0, 4, 12, 8, 12);
     }
 
-
+    public BlockState getPlacementState(ItemPlacementContext ctx) {
+        return (BlockState)this.getDefaultState().with(FACING, ctx.getPlayerFacing().getOpposite());
+    }
 }
