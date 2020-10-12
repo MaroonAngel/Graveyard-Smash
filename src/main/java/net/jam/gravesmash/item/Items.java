5bf70d1a7cadbea9ac1ae02e4f3e97b24b0ecaa8
Tybie <tybiemeyer@gmail.com>
2020-10-12 12:40:20 -0400

    Autumn Essence Item

--- src/main/java/net/jam/gravesmash/item/Items.java
+++ src/main/java/net/jam/gravesmash/item/Items.java
@@ -9,10 +9,12 @@ public class Items {
     // Seeds
     public static final Item WHITE_PUMPKIN_SEEDS = new Item(new Item.Settings().group(ItemGroup.MISC));
     public static final Item GREEN_PUMPKIN_SEEDS = new Item(new Item.Settings().group(ItemGroup.MISC));
+    public static final Item AUTUMN_ESSENCE = new Item(new Item.Settings().group(ItemGroup.MISC));
 
     // Registry
     public static void register() {
     Registry.register(Registry.ITEM, new Identifier("gravesmash", "white_pumpkin_seeds"), (WHITE_PUMPKIN_SEEDS));
-        Registry.register(Registry.ITEM, new Identifier("gravesmash", "green_pumpkin_seeds"), (GREEN_PUMPKIN_SEEDS));
+    Registry.register(Registry.ITEM, new Identifier("gravesmash", "green_pumpkin_seeds"), (GREEN_PUMPKIN_SEEDS));
+    Registry.register(Registry.ITEM, new Identifier("gravesmash", "autumn_essence"), (AUTUMN_ESSENCE));
  }
 }
